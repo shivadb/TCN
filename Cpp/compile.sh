2b4971e278297bbb0e5e9962db41254e4a218bdf
Shiv <58155330+shivadb@users.noreply.github.com>
2021-07-23 01:11:21 -0700

    cpp working

--- Cpp/compile.sh
+++ Cpp/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ main.cpp logger.cpp -I/usr/local/cuda/include -L/usr/lib/x86_64-linux-gnu -lnvinfer -o Test
+g++ main.cpp logger.cpp -I/usr/local/cuda/include -L/usr/lib/x86_64-linux-gnu -lnvinfer -L/usr/local/cuda/lib64 -lcudart -o Test
--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <vector>
 #include <typeinfo>
+#include <cuda_runtime_api.h>
 
 #include <NvInfer.h>
 #include <NvInferPlugin.h>
@@ -29,6 +30,12 @@ struct TRTDestroy {
 
 template< class T >
 using TRTUniquePtr = std::unique_ptr< T, TRTDestroy >;
+
+void printVec(std::vector <float> const &a) {
+   for(int i=0; i < a.size(); i++)
+   std::cout << a.at(i) << ' ';
+   std::cout << std::endl;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -72,12 +79,34 @@ int main(int argc, char const *argv[])
     
     context.reset(engine->createExecutionContext());
 
+    std::vector<float> output(10, 0);
+    std::vector<float> testInput(784, 1);
+    // std::cout << "Test vector: " << testInput.data() << std::endl;
+    // printVec(testInput);
+
+    void* buffers[2];
     int inputIndex = engine->getBindingIndex("input_0");
     int outputIndex = engine->getBindingIndex("output_0");
 
     // std::cout << "Input idx: " << inputIndex << " Output idx: " << outputIndex << std::endl;
 
-    
+    cudaMalloc(&buffers[inputIndex], 784 * sizeof(float));
+    cudaMalloc(&buffers[outputIndex], 10 * sizeof(float));
+
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
+
+    cudaMemcpyAsync(buffers[inputIndex], testInput.data(), 784 * sizeof(float), cudaMemcpyHostToDevice, stream);
+    context->enqueue(1, buffers, stream, nullptr);
+    cudaMemcpyAsync(output.data(), buffers[outputIndex], 10 * sizeof(float), cudaMemcpyDeviceToHost, stream);
+    cudaStreamSynchronize(stream);
+
+    std::cout << "Output: " << std::endl;
+    printVec(output);
+
+    cudaStreamDestroy(stream);
+    cudaFree(buffers[inputIndex]);
+    cudaFree(buffers[outputIndex]);
 
     return 0;
 }
